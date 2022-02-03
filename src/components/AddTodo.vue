25c8045e8a33317974ea181558ec91fa450248e8
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 14:06:34 +0300

    addTodo action added to AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Add Todo</h3>
     <div class="add">
-      <form>
+      <form @submit="onSubmit">
         <input type="text" v-model="title" placeholder="Add Todo..." />
         <input type="submit" value="Submit" />
       </form>
@@ -11,8 +11,22 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    ...mapActions(["addTodo"]),
+    onSubmit(e) {
+      e.preventDefault();
+      this.addTodo(this.title);
+      this.title = "";
+    },
+  },
 };
 </script>
 
