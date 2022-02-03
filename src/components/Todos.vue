79e01b2d4250210776da3c21ff6408b189ab147d
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 11:57:08 +0300

    Todos created and styled

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{ todo.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "Todos",
+  computed: mapGetters(["allTodos"]),
 };
 </script>
 
-<style></style>
+<style>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b882;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
