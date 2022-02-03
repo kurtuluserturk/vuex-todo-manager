819c31306d7991e7fef83c67dfb4a2e044927c68
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 19:11:02 +0300

    filterTodos added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="container">
       <AddTodo />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
@@ -10,12 +11,14 @@
 <script>
 import Todos from "./components/Todos.vue";
 import AddTodo from "./components/AddTodo.vue";
+import FilterTodos from "./components/FilterTodos.vue";
 
 export default {
   name: "App",
   components: {
     Todos,
     AddTodo,
+    FilterTodos,
   },
 };
 </script>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -22,6 +22,15 @@ const actions = {
   async deleteTodo({ commit }, id) {
     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
     commit('removeTodo', id)
+  },
+
+  async filterTodos({ commit }, e) {
+    // Get selected number
+    const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText)
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
+
+    commit('setTodos', response.data)
   }
 }
 
