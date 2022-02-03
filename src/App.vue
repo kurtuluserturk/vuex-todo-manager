f00578a69da58dfef1ac29b36c87966db891f832
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 13:28:41 +0300

    AddTodo component created

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo />
       <Todos />
     </div>
   </div>
@@ -8,11 +9,13 @@
 
 <script>
 import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
 
 export default {
   name: "App",
   components: {
     Todos,
+    AddTodo,
   },
 };
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form>
+        <input type="text" v-model="title" placeholder="Add Todo..." />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+};
+</script>
+
+<style></style>
