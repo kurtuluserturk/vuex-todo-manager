016adebed131599cc3c6d74fb12127fa0d5eb72b
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 14:38:15 +0300

    deleteTodo action was added

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,7 +4,7 @@
     <div class="todos">
       <div v-for="todo in allTodos" :key="todo.id" class="todo">
         {{ todo.title }}
-        <i class="fas fa-trash-alt"></i>
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -15,7 +15,7 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos"]),
+    ...mapActions(["fetchTodos", "deleteTodo"]),
   },
   computed: mapGetters(["allTodos"]),
   created() {
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -17,12 +17,18 @@ const actions = {
   async addTodo({ commit }, title) {
     const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false })
     commit('addNewTodo', response.data)
+  },
+
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    commit('removeTodo', id)
   }
 }
 
 const mutations = {
   setTodos: (state, todos) => state.todos = todos,
-  addNewTodo: (state, todo) => state.todos.unshift(todo)
+  addNewTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
 }
 
 export default {
