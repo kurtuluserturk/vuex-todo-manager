18662479bec27f0412a94275e0a7030b2d0c0009
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-03 13:31:15 +0300

    Inputs were styled

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -16,4 +16,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+  border: 1px solid #41b883;
+  outline: 0;
+}
+
+input[type="submit"] {
+  flex: 2;
+  background: #41b883;
+  color: #fff;
+  border: 1px #41b883 solid;
+  cursor: pointer;
+}
+</style>
